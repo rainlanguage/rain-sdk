297e32130ca540471632c5292ae3bc3683ddc776
Josh Hardy <jks.hardy@gmail.com>
2021-12-01 14:33:59 +0000

    lint

--- src/trust.ts
+++ src/trust.ts
@@ -1,115 +1,163 @@
-import { Token } from '@uniswap/sdk-core'
-import { ethers, Signer, ContractTransaction, ContractReceipt } from 'ethers'
-import { Provider } from '@ethersproject/providers'
+import { Token } from '@uniswap/sdk-core';
+import { ethers, Signer, ContractTransaction, ContractReceipt } from 'ethers';
+import { Provider } from '@ethersproject/providers';
 import {
-    Trust as TrustContract,
-    Trust__factory,
-    ReadOnlyTier,
-    ReadOnlyTier__factory,
-    TrustFactory__factory,
-} from '@beehiveinnovation/rain-protocol/typechain'
-import { Reserve } from './reserve'
-import { Redeemable } from './redeemable'
-import { RedeemableERC20Pool } from './redeemableERC20Pool'
-import { SeedERC20 } from './seeder'
-import { CRP } from './crp'
-import { Pool } from './pool'
-import { Config } from './types'
-import { AddressBook } from './addresses'
+  Trust as TrustContract,
+  Trust__factory,
+  ReadOnlyTier,
+  ReadOnlyTier__factory,
+  TrustFactory__factory,
+} from '@beehiveinnovation/rain-protocol/typechain';
+import { Reserve } from './reserve';
+import { Redeemable } from './redeemable';
+import { RedeemableERC20Pool } from './redeemableERC20Pool';
+import { SeedERC20 } from './seeder';
+import { CRP } from './crp';
+import { Pool } from './pool';
+import { Config } from './types';
+import { AddressBook } from './addresses';
 
 type TrustContracts = {
-    trust: TrustContract
-    reserve: Reserve
-    redeemable: Redeemable
-    redeemableERC20Pool: RedeemableERC20Pool
-    seeder: SeedERC20 | string
-    tier: ReadOnlyTier | any
-    crp: CRP | null
-    pool: Pool | null
-}
+  trust: TrustContract;
+  reserve: Reserve;
+  redeemable: Redeemable;
+  redeemableERC20Pool: RedeemableERC20Pool;
+  seeder: SeedERC20 | string;
+  tier: ReadOnlyTier | any;
+  crp: CRP | null;
+  pool: Pool | null;
+};
 
 export class Trust {
+  public readonly chainId: number;
+  public readonly readOnly: boolean;
+  public readonly signerOrProvider: Signer | Provider;
+  public reserve: Reserve;
+  public redeemable: Redeemable;
+  public redeemablePool: RedeemableERC20Pool;
+  public seeder: SeedERC20 | string;
+  public crp: CRP | null;
+  public pool: Pool | null;
 
-    public readonly chainId: number
-    public readonly readOnly: boolean
-    public readonly signerOrProvider: Signer | Provider
-    public reserve: Reserve
-    public redeemable: Redeemable
-    public redeemablePool: RedeemableERC20Pool
-    public seeder: SeedERC20 | string
-    public crp: CRP | null
-    public pool: Pool | null
+  protected constructor(
+    contracts: TrustContracts,
+    signerOrProvider: Signer | Provider,
+    chainId: number
+  ) {
+    this.chainId = chainId;
+    this.readOnly = !Signer.isSigner(signerOrProvider);
+    this.signerOrProvider = signerOrProvider;
 
-    protected constructor(contracts: TrustContracts, signerOrProvider: Signer | Provider, chainId: number) {
+    this.reserve = contracts.reserve;
+    this.redeemable = contracts.redeemable;
+    this.redeemablePool = contracts.redeemableERC20Pool;
+    this.seeder = contracts.seeder;
+    this.crp = contracts.crp;
+    this.pool = contracts.pool;
 
-        this.chainId = chainId
-        this.readOnly = !Signer.isSigner(signerOrProvider)
-        this.signerOrProvider = signerOrProvider
+    Object.assign(this, contracts.trust);
+  }
 
-        this.reserve = contracts.reserve
-        this.redeemable = contracts.redeemable
-        this.redeemablePool = contracts.redeemableERC20Pool
-        this.seeder = contracts.seeder
-        this.crp = contracts.crp
-        this.pool = contracts.pool
-
-        Object.assign(this, contracts.trust)
-
+  public static async fromAddress(
+    address: string,
+    signerOrProvider: Signer | Provider,
+    chainId: number
+  ): Promise<Trust> {
+    if (!ethers.utils.isAddress(address)) {
+      throw Error('Address is not a valid address');
     }
 
-    public static async fromAddress(address: string, signerOrProvider: Signer | Provider, chainId: number): Promise<Trust> {
+    const trust = Trust__factory.connect(address, signerOrProvider);
+    const addresses = await trust.getContracts(); // @TODO change to subgraph call
+    const reserve = await Reserve.fromAddress(
+      addresses.reserveERC20,
+      signerOrProvider,
+      chainId
+    );
+    const redeemable = await Redeemable.fromAddress(
+      addresses.redeemableERC20,
+      signerOrProvider,
+      chainId
+    );
+    const redeemableERC20Pool = await RedeemableERC20Pool.fromAddress(
+      addresses.redeemableERC20Pool,
+      signerOrProvider,
+      chainId
+    );
+    const seeder = await SeedERC20.fromAddress(
+      addresses.seeder,
+      signerOrProvider,
+      chainId
+    );
+    const crp =
+      addresses.crp !== ethers.constants.AddressZero
+        ? await CRP.fromAddress(addresses.crp, signerOrProvider, chainId)
+        : null;
+    const pool =
+      addresses.pool !== ethers.constants.AddressZero
+        ? await Pool.fromAddress(addresses.pool, signerOrProvider, chainId)
+        : null;
 
-        if (!ethers.utils.isAddress(address)) {
-            throw "Address is not a valid address"
-        }
+    const tier = ReadOnlyTier__factory.connect(
+      addresses.tier,
+      signerOrProvider
+    );
 
-        const trust = Trust__factory.connect(address, signerOrProvider)
-        const addresses = await trust.getContracts() // @TODO change to subgraph call
-        const reserve = await Reserve.fromAddress(addresses.reserveERC20, signerOrProvider, chainId)
-        const redeemable = await Redeemable.fromAddress(addresses.redeemableERC20, signerOrProvider, chainId)
-        const redeemableERC20Pool = await RedeemableERC20Pool.fromAddress(addresses.redeemableERC20Pool, signerOrProvider, chainId)
-        const seeder = await SeedERC20.fromAddress(addresses.seeder, signerOrProvider, chainId)
-        const crp = addresses.crp !== ethers.constants.AddressZero ? await CRP.fromAddress(addresses.crp, signerOrProvider, chainId) : null
-        const pool = addresses.pool !== ethers.constants.AddressZero ? await Pool.fromAddress(addresses.pool, signerOrProvider, chainId) : null
+    return new Trust(
+      {
+        trust,
+        reserve,
+        redeemable,
+        redeemableERC20Pool,
+        seeder,
+        tier,
+        crp,
+        pool,
+      },
+      signerOrProvider,
+      chainId
+    );
+  }
 
-        const tier = ReadOnlyTier__factory.connect(addresses.tier, signerOrProvider)
+  public static async fromTransactionReceipt(
+    receipt: ContractReceipt,
+    signerOrProvider: Signer | Provider,
+    chainId: number
+  ): Promise<Trust> {
+    const trustAddress = receipt.events?.filter(
+      x => x.event === 'NewContract'
+    )[0].topics[1];
 
-        return new Trust({
-            trust,
-            reserve,
-            redeemable,
-            redeemableERC20Pool,
-            seeder,
-            tier,
-            crp,
-            pool
-        }, signerOrProvider, chainId)
+    if (trustAddress === undefined) {
+      throw Error('Event "NewContract" is not in transaction receipt.');
     }
 
-    public static async fromTransactionReceipt(receipt: ContractReceipt, signerOrProvider: Signer | Provider, chainId: number): Promise<Trust> {
-        const trustAddress = receipt.events?.filter(
-            (x) => x.event == "NewContract"
-        )[0].topics[1]
+    return this.fromAddress(trustAddress, signerOrProvider, chainId);
+  }
 
-        if (trustAddress == undefined) {
-            throw ('Event "NewContract" is not in transaction receipt.')
-        }
+  public static async createTrust(
+    config: Config,
+    signer: Signer,
+    chainId: number
+  ): Promise<ContractTransaction> {
+    const trustFactory = TrustFactory__factory.connect(
+      AddressBook.getFactoryAddressFromChainId(chainId),
+      signer
+    );
+    const tx = await trustFactory[
+      'createChild((address,uint256,address,uint256,uint16,uint16,uint256),(string,string,address,uint8,uint256),(address,uint256,uint256,uint256,uint256))'
+    ](
+      config.trustConfig,
+      config.redeemableERC20Config,
+      config.redeemableERC20PoolConfig
+    );
+    return tx;
+  }
 
-        return this.fromAddress(trustAddress, signerOrProvider, chainId)
-    }
-
-    public static async createTrust(config: Config, signer: Signer, chainId: number): Promise<ContractTransaction> {
-        const trustFactory = TrustFactory__factory.connect(AddressBook.getFactoryAddressFromChainId(chainId), signer)
-        const tx = await trustFactory['createChild((address,uint256,address,uint256,uint16,uint16,uint256),(string,string,address,uint8,uint256),(address,uint256,uint256,uint256,uint256))'](config.trustConfig, config.redeemableERC20Config, config.redeemableERC20PoolConfig)
-        return tx;
-    }
-
-    /**
-     * Helper method for returning both tokens.
-     */
-    public async tokens(): Promise<[Token, Token]> {
-        return [await this.reserve.getToken(), await this.redeemable.getToken()]
-    }
+  /**
+   * Helper method for returning both tokens.
+   */
+  public async tokens(): Promise<[Token, Token]> {
+    return [await this.reserve.getToken(), await this.redeemable.getToken()];
+  }
 }
-
-
