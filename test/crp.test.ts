099dd4157d66ab7898e58699f43a236b5ac22ea0
Timothy Coleman <timhc22@gmail.com>
2022-01-28 05:25:00 +0000

    Create beggings of test for crp

---
+++ test/crp.test.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai';
+import { CRP } from '../src/crp';
+// @ts-ignore
+import { ethers } from "hardhat";
+
+describe('Configurable Rights Pool', () => {
+
+  it('returns fromAddress', async () => {
+
+    // const signers = await ethers.getSigners();
+    // const creator = signers[0];
+
+    // let result = await CRP.fromAddress('1', creator , 1);
+    // expect(result).to.be.equal('');
+  });
+
+});
