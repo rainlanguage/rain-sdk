117ec9c8cf73ec03ca5b9cdc9e9cdb617ea28e0a
Josh Hardy <jks.hardy@gmail.com>
2022-03-31 19:41:12 +0100

    updating docs

--- src/sale.ts
+++ src/sale.ts
@@ -1,4 +1,4 @@
-import { Signer } from 'ethers';
+import { BigNumberish, Signer } from 'ethers';
 import { AddressBook } from './addresses';
 import { RainContract } from './rain-contract';
 import {
@@ -7,31 +7,68 @@ import {
   SaleFactory__factory,
 } from './typechain/rain-protocol';
 import {
-  SaleConfigStruct,
   SaleRedeemableERC20ConfigStruct,
+  StateConfigStruct,
 } from './typechain/rain-protocol/Sale';
 
-type SaleDeployArgs = {
+interface SaleConfigStruct {
+  /**
+   * The state config for 'can start'
+   */
+  canStartStateConfig: StateConfigStruct;
+  canEndStateConfig: StateConfigStruct;
+  calculatePriceStateConfig: StateConfigStruct;
+  recipient: string;
+  reserve: string;
+  cooldownDuration: BigNumberish;
+  minimumRaise: BigNumberish;
+  dustSize: BigNumberish;
+}
+interface SaleDeployArguments {
+  /**
+   * Config for the Sale
+   */
   _config: SaleConfigStruct;
+  /**
+   * Config for the RedeemableERC20 token that is minted by the Sale
+   */
   saleRedeemableERC20Config_: SaleRedeemableERC20ConfigStruct;
-};
+}
 
 export class Sale extends RainContract {
   public readonly signer: Signer;
   public readonly sale!: SaleContract;
   public deploy: Function;
 
+  /**
+   * Constructs a new Sale from a known address.
+   *
+   * @param address - The address of the Sale contract
+   * @param signer - An ethers.js Signer
+   * @returns A new Sale instance
+   *
+   */
   constructor(address: string, signer: Signer) {
     super();
     this.signer = signer;
     this.sale = Sale__factory.connect(address, signer);
   }
 
+  /**
+   * Deploys a new Sale.
+   *
+   * @param signer - An ethers.js Signer
+   * @param chainId - The chain id of the network (e.g. 80001)
+   * @param args - Arguments for deploying a Sale @see SaleDeployArguments
+   * @returns A new Sale instance
+   *
+   */
   public static deploy = async (
     signer: Signer,
     chainId: number,
-    { _config, saleRedeemableERC20Config_ }: SaleDeployArgs
+    args: SaleDeployArguments
   ) => {
+    const { _config, saleRedeemableERC20Config_ } = args;
     const saleFactory = SaleFactory__factory.connect(
       AddressBook.getAddressesForChainId(chainId).gatedNFT,
       signer
