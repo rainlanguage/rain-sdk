040b8fd8aa4bcb03ca175b3d6e71ee2dcf3a5613
NanezX <victor.nanezj@gmail.com>
2022-04-14 02:05:14 -0400

    basic testing

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,19 +1,38 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
-import { TierLevels, Addresses } from './utils';
+import {
+  chainId,
+  Tier,
+  TierLevels,
+  Addresses,
+  expectAsyncError,
+} from './utils';
 
-import { ERC20BalanceTier } from '../dist';
-
-/**
- * Hardhat network chainID
- */
-export const chainId = 31337;
+// import { ERC20BalanceTier, ERC20TransferTier, Verify } from '../dist';
+import {
+  ERC20BalanceTier,
+  ERC20TransferTier,
+  ERC721BalanceTier,
+  VerifyTier,
+  Verify,
+} from '../src';
 
 /**
  * Addresses saved that are in SDK BookAddresses deployed to Hardhat network.
  * **These addresses are deterministically generated with the HH signers.**
  */
 export let addresses: Addresses;
+
+// Meanwhile
+async function deployErc20() {
+  const TokenFactory = await ethers.getContractFactory('ReserveTokenTest');
+  return await TokenFactory.deploy();
+}
+
+async function deployErc721() {
+  const TokenFactory = await ethers.getContractFactory('ReserveTokenERC721');
+  return await TokenFactory.deploy();
+}
 
 before('Initializing and deploying contracts to hardhat network', async () => {
   // ⚠️ Contract Factories instances ⚠️
@@ -106,16 +125,129 @@ before('Initializing and deploying contracts to hardhat network', async () => {
 });
 
 describe('SDK Test', () => {
-  it('Deploying TierChild', async () => {
-    const TokenFactory = await ethers.getContractFactory('ReserveTokenTest');
-    const token = await TokenFactory.deploy();
+  describe('Verify', () => {
+    // Verify Roles.
+    // TODO: Is it redundant? Because it is the same that the class or how will be the best way to test this?
+    const DEFAULT_ADMIN_ROLE = ethers.utils.hexZeroPad('0x00', 32);
 
-    const [signer] = await ethers.getSigners();
-    const tier = await ERC20BalanceTier.deploy(signer, chainId, {
-      erc20: token.address,
-      tierValues: TierLevels,
+    const APPROVER = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes('APPROVER')
+    );
+    const REMOVER = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('REMOVER'));
+    const BANNER = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BANNER'));
+
+    const APPROVER_ADMIN = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes('APPROVER_ADMIN')
+    );
+    const REMOVER_ADMIN = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes('REMOVER_ADMIN')
+    );
+
+    const BANNER_ADMIN = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes('BANNER_ADMIN')
+    );
+
+    it('Deploying VerifyChild', async () => {
+      // TODO: Separete expects as unit testing
+      const [signer] = await ethers.getSigners();
+      const verify = await Verify.deploy(signer, chainId, {
+        admin: signer.address,
+        callback: ethers.constants.AddressZero,
+      });
+
+      expect(verify.DEFAULT_ADMIN_ROLE).to.equals(DEFAULT_ADMIN_ROLE);
+
+      expect(verify.APPROVER).to.equals(APPROVER);
+      expect(verify.REMOVER).to.equals(REMOVER);
+      expect(verify.BANNER).to.equals(BANNER);
+
+      expect(verify.APPROVER_ADMIN).to.equals(APPROVER_ADMIN);
+      expect(verify.REMOVER_ADMIN).to.equals(REMOVER_ADMIN);
+      expect(verify.BANNER_ADMIN).to.equals(BANNER_ADMIN);
+
+      expect(await verify.callback()).to.equals(ethers.constants.AddressZero);
+
+      const erc165Id = '0x01ffc9a7';
+      expect(await verify.supportsInterface(erc165Id)).to.be.true;
+      expect(await verify.supportsInterface('0xffffffff')).to.be.false;
+    });
+  });
+
+  describe('Tiers', () => {
+    // TODO: search the script to decode reports
+    // console.log((await tier.report(ethers.constants.AddressZero)).toString());
+
+    it('Deploying ERC20BalanceTierChild', async () => {
+      const token = await deployErc20();
+
+      const [signer] = await ethers.getSigners();
+      const tier = await ERC20BalanceTier.deploy(signer, chainId, {
+        erc20: token.address,
+        tierValues: TierLevels,
+      });
+
+      expect(TierLevels).to.be.deep.equal(await tier.tierValues());
+      await expectAsyncError(
+        tier.setTier('', 2, []),
+        'SET TIER: NOT IMPLEMENTED'
+      );
     });
 
-    expect(TierLevels).to.be.deep.equal(await tier.tierValues());
+    it('Deploying ERC20TransferTierChild', async () => {
+      const token = await deployErc20();
+
+      const [signer] = await ethers.getSigners();
+      const tier = await ERC20TransferTier.deploy(signer, chainId, {
+        erc20: token.address,
+        tierValues: TierLevels,
+      });
+
+      expect(TierLevels).to.be.deep.equal(await tier.tierValues());
+
+      const requiredTierFive = TierLevels[4];
+
+      await token.transfer(tier.address, requiredTierFive);
+      await token.connect(signer).approve(tier.address, requiredTierFive);
+
+      const beforeSet = await tier.report(signer.address);
+
+      await tier.setTier(signer.address, Tier.FIVE, []);
+
+      expect(await tier.report(signer.address)).to.be.not.equals(beforeSet);
+    });
+
+    it('Deploying ERC721BalanceTierChild', async () => {
+      const token = await deployErc721();
+
+      const [signer] = await ethers.getSigners();
+      const tier = await ERC721BalanceTier.deploy(signer, chainId, {
+        erc721: token.address,
+        tierValues: TierLevels,
+      });
+
+      expect(TierLevels).to.be.deep.equal(await tier.tierValues());
+      await expectAsyncError(
+        tier.setTier('', 2, []),
+        'SET TIER: NOT IMPLEMENTED'
+      );
+    });
+
+    it('Deploying VerifyTierChild', async () => {
+      const [signer] = await ethers.getSigners();
+
+      const verify = await Verify.deploy(signer, chainId, {
+        admin: signer.address,
+        callback: ethers.constants.AddressZero,
+      });
+
+      const tier = await VerifyTier.deploy(signer, chainId, verify.address);
+
+      await expectAsyncError(
+        tier.setTier('', 2, []),
+        'SET TIER: NOT IMPLEMENTED'
+      );
+    });
   });
+
+  describe('test', () => {});
 });
--- test/utils.ts
+++ test/utils.ts
@@ -1,7 +1,28 @@
 import { ethers } from 'hardhat';
 import { BigNumberish } from 'ethers';
+import { assert } from 'chai';
 
 export const eighteenZeros = '000000000000000000';
+
+/**
+ * Hardhat network chainID
+ */
+export const chainId = 31337;
+
+/**
+ * Enum for each Tier
+ */
+export enum Tier {
+  ZERO,
+  ONE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+}
 
 /**
  * Return the Levels tier used by default. LEVELS always will be an array with 8 elements to
@@ -28,4 +49,43 @@ export interface Addresses {
   NoticeBoard: string;
   EmissionsERC20Factory: string;
   SaleFactory: string;
+}
+
+/**
+ * Expect that a async function/Promise throw an Error. Should be use only for
+ * JS errors. To catch error in EVM, should use the ethereum-waffle library.
+ *
+ * @param cb - async functon or Promise (same)
+ * @param errorMsg - (optional) Error message that is expected
+ */
+export async function expectAsyncError(
+  cb: Promise<unknown>,
+  errorMsg?: string
+): Promise<void> {
+  try {
+    await cb;
+    assert(false, 'no error was throw');
+  } catch (e) {
+    const message = e instanceof Error ? e.message : 'Unknown Error';
+
+    if (message === 'no error was throw') {
+      if (errorMsg) {
+        assert(false, `Expected an error with "${errorMsg}" but ${message}`);
+      }
+      assert(false, message);
+    }
+
+    if (!errorMsg) {
+      return;
+    }
+
+    if (errorMsg === message) {
+      return;
+    } else {
+      assert(
+        false,
+        `Expected an error with "${errorMsg}" but got "${message}"`
+      );
+    }
+  }
 }
