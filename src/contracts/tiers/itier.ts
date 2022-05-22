4c09bef8090065d302515d50bca52feb132febf8
NanezX <victor.nanezj@gmail.com>
2022-05-21 21:26:20 -0400

    adding itier

---
+++ src/contracts/tiers/itier.ts
@@ -0,0 +1,17 @@
+import { Signer } from 'ethers';
+import { TierContract } from './../../classes/tierContract';
+
+/**
+ * Class to interact with ITier contracts
+ *
+ * @remarks
+ * Generic class to interact with any ITier contract in chain with the basic methods and functions.
+ *
+ * `ITier` is a simple interface that contracts can implement to provide membership lists for other contracts.
+ *
+ */
+export class ITier extends TierContract {
+  public readonly connect = (signer: Signer): ITier => {
+    return new ITier(this.address, signer);
+  };
+}
