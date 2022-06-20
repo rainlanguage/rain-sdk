17a7a72339d14cff1dd00c16b5d9a5b5173f6488
rouzwelt <98348398+rouzwelt@users.noreply.github.com>
2022-06-20 21:00:56 +0000

    BalanceTier templates

--- src/contracts/script-generators/combineTierScriptGenerator.ts
+++ src/contracts/script-generators/combineTierScriptGenerator.ts
@@ -12,6 +12,7 @@ import {
   paddedUInt256,
   selectLteLogic,
   selectLteMode,
+  parseUnits,
 } from '../../utils';
 
 /**
@@ -56,7 +57,7 @@ export class CombineTierGenerator {
    *
    * @returns this
    */
-  public combine(
+  public combineWith(
     reportVar: string | StateConfig,
     logic: selectLteLogic,
     mode: selectLteMode,
@@ -95,7 +96,7 @@ export class CombineTierGenerator {
    *
    * @returns this
    */
-  public updateReportAtTierRange(
+  public updateReport(
     startTier: Tier,
     endTier: Tier,
     number?: number
@@ -128,7 +129,7 @@ export class CombineTierGenerator {
    *
    * @returns this
    */
-  public satDiff(reportVar: string | StateConfig): this {
+  public differenceFrom(reportVar: string | StateConfig): this {
     const _buttom: StateConfig =
       typeof reportVar == 'string'
         ? new CombineTierGenerator(reportVar)
@@ -156,7 +157,7 @@ export class CombineTierGenerator {
    *
    * @returns this
    */
-  public holdingTimeTransferTier(
+  public isTierHeldFor(
     reportVar: string | StateConfig,
     numberOfBlocks: number | number[]
   ): this {
@@ -166,8 +167,6 @@ export class CombineTierGenerator {
         : reportVar;
 
     const _shifter = paddedUInt256(
-      BigNumber.from(
-        '0x' +
           paddedUInt32('7') +
           paddedUInt32('6') +
           paddedUInt32('5') +
@@ -176,7 +175,6 @@ export class CombineTierGenerator {
           paddedUInt32('2') +
           paddedUInt32('1') +
           paddedUInt32('0')
-      )
     );
 
     const _blocks = paddedUInt256(
@@ -246,6 +244,7 @@ export class CombineTierGenerator {
           op(VM.Opcodes.CONSTANT, 2),
           op(VM.Opcodes.CONSTANT, 7),
           op(VM.Opcodes.EXP, 2),
+          op(VM.Opcodes.MUL, 2),
         ]),
       ],
     };
@@ -355,5 +354,147 @@ export class BuildReport extends CombineTierGenerator {
       };
     }
     super(_result);
+  }
+};
+
+/**
+ * @public
+ * class to create a the vmStateConfig for CombineTier as BalanceTier.
+ * this will perform similar to ERC20BalancTier in RainVM version 1.0
+ * 
+ */
+export class ERC20BalanceTier extends CombineTierGenerator {
+
+  /**
+   * Constructor for ERC20BalanceTier vmStateConfig
+   * 
+   * @param tierValues - an array of 8 values for each tier values
+   * @param tokenAddress - the ERC20 token address
+   * @param tokenDecimals - the ERC20 token decimals
+   */
+  constructor (
+    public readonly tierValues: (number | string)[],
+    public readonly tokenAddress: string,
+    tokenDecimals: number = 18
+  ) {
+    const constants = [
+      paddedUInt256(
+        paddedUInt32(parseUnits(tierValues[7].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[6].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[5].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[4].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[3].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[2].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[1].toString(), tokenDecimals)) +
+        paddedUInt32(parseUnits(tierValues[0].toString(), tokenDecimals))
+      ),
+      tokenAddress,
+      paddedUInt256(
+        paddedUInt32('7') +
+        paddedUInt32('6') +
+        paddedUInt32('5') +
+        paddedUInt32('4') +
+        paddedUInt32('3') +
+        paddedUInt32('2') +
+        paddedUInt32('1') +
+        paddedUInt32('0')
+      ),
+      "0xffffffff",
+      "0",
+      "100000000"
+    ];
+     const sources = [
+      concat([
+        op(VM.Opcodes.CONSTANT, 0),
+        op(VM.Opcodes.CONSTANT, 2),
+        op(VM.Opcodes.ZIPMAP, callSize(1, 3, 1)),
+        op(VM.Opcodes.ADD, 8),
+      ]),
+      concat([
+        op(VM.Opcodes.CONSTANT, 1),
+        op(VM.Opcodes.CONTEXT, 0),
+        op(VM.Opcodes.IERC20_BALANCE_OF),
+        op(VM.Opcodes.CONSTANT, 6),
+        op(VM.Opcodes.LESS_THAN),
+        op(VM.Opcodes.CONSTANT, 3),
+        op(VM.Opcodes.CONSTANT, 4),
+        op(VM.Opcodes.EAGER_IF),
+        op(VM.Opcodes.CONSTANT, 5),
+        op(VM.Opcodes.CONSTANT, 7),
+        op(VM.Opcodes.EXP, 2),
+        op(VM.Opcodes.MUL, 2),
+      ])
+    ];
+    super({constants, sources});
+  }
+};
+
+/**
+ * @public
+ * class to create a the vmStateConfig for CombineTier as BalanceTier.
+ * this will perform similar to ERC721BalancTier in RainVM version 1.0
+ * 
+ */
+ export class ERC721BalanceTier extends CombineTierGenerator {
+
+  /**
+   * Constructor for ERC721BalanceTier vmStateConfig
+   * 
+   * @param tierValues - an array of 8 values for each tier values
+   * @param tokenAddress - the ERC721 token address
+   */
+  constructor (
+    public readonly tierValues: (number | string)[],
+    public readonly tokenAddress: string,
+  ) {
+    const constants = [
+      paddedUInt256(
+        paddedUInt32(tierValues[7].toString()) +
+        paddedUInt32(tierValues[6].toString()) +
+        paddedUInt32(tierValues[5].toString()) +
+        paddedUInt32(tierValues[4].toString()) +
+        paddedUInt32(tierValues[3].toString()) +
+        paddedUInt32(tierValues[2].toString()) +
+        paddedUInt32(tierValues[1].toString()) +
+        paddedUInt32(tierValues[0].toString())
+      ),
+      tokenAddress,
+      paddedUInt256(
+        paddedUInt32('7') +
+        paddedUInt32('6') +
+        paddedUInt32('5') +
+        paddedUInt32('4') +
+        paddedUInt32('3') +
+        paddedUInt32('2') +
+        paddedUInt32('1') +
+        paddedUInt32('0')
+      ),
+      "0xffffffff",
+      "0",
+      "100000000"
+    ];
+     const sources = [
+      concat([
+        op(VM.Opcodes.CONSTANT, 0),
+        op(VM.Opcodes.CONSTANT, 2),
+        op(VM.Opcodes.ZIPMAP, callSize(1, 3, 1)),
+        op(VM.Opcodes.ADD, 8),
+      ]),
+      concat([
+        op(VM.Opcodes.CONSTANT, 1),
+        op(VM.Opcodes.CONTEXT, 0),
+        op(VM.Opcodes.IERC721_BALANCE_OF),
+        op(VM.Opcodes.CONSTANT, 6),
+        op(VM.Opcodes.LESS_THAN),
+        op(VM.Opcodes.CONSTANT, 3),
+        op(VM.Opcodes.CONSTANT, 4),
+        op(VM.Opcodes.EAGER_IF),
+        op(VM.Opcodes.CONSTANT, 5),
+        op(VM.Opcodes.CONSTANT, 7),
+        op(VM.Opcodes.EXP, 2),
+        op(VM.Opcodes.MUL, 2),
+      ])
+    ];
+    super({constants, sources});
   }
 }
