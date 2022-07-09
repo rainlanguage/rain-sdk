78071d59ffcfa589ac9d70da4ba537e99a839bdf
rouzwelt <98348398+rouzwelt@users.noreply.github.com>
2022-07-09 21:20:11 +0000

    template decimal fix

--- src/contracts/script-generators/saleScriptGenerator.ts
+++ src/contracts/script-generators/saleScriptGenerator.ts
@@ -404,7 +404,7 @@ export class vLBP extends PriceCurve {
       constants: [
         parseUnits(balanceReserve.toString(), erc20decimals),
         parseUnits(initWeight.toString()),
-        parseUnits(weightChange.toFixed(5).toString()),
+        parseUnits(weightChange.toFixed(erc20decimals).toString()),
         startTimestamp,
         parseUnits((1).toString()),
       ],
@@ -472,7 +472,7 @@ export class IncreasingPrice extends PriceCurve {
       constants: [
         parseUnits(startPrice.toString(), erc20decimals),
         parseUnits(endPrice.toString(), erc20decimals),
-        parseUnits(priceChange.toFixed(5).toString(), erc20decimals),
+        parseUnits(priceChange.toFixed(erc20decimals).toString(), erc20decimals),
         startTimestamp,
       ],
       sources: [IncreasingPrice.INC_PRICE_SOURCES()],
